70cf52715dd9fb85d94246f48505dbeb173bd347
Brandon Selzer <bselzer1@hotmail.com>
2018-04-26 05:49:16 -0400

    Created OnItemClickListener() for the ListView in PackPackActivity. Minor layout adjustments.

--- app/src/main/java/com/team2/packpackmonsters/IndividualPackPackActivity.java
+++ app/src/main/java/com/team2/packpackmonsters/IndividualPackPackActivity.java
@@ -2,14 +2,19 @@ package com.team2.packpackmonsters;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class IndividualPackPackActivity extends AppCompatActivity
 {
+    public static final String PACK_PACK_KEY = "Pack Pack";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_pack_pack);
+
+        TextView indPackTxtName = findViewById(R.id.ind_pack_txt_name);
+        indPackTxtName.setText(getIntent().getStringExtra(PACK_PACK_KEY));
     }
 }
