932903cdb1bc7471ddce240eb2c902b6940a33c0
Brandon Selzer <bselzer1@hotmail.com>
2018-04-29 19:03:01 -0400

    When going to BattleActivity, party screen comes up and forces a selection before being able to do anything else, including leave. Put initialization of party views in a loop. Name is added to health label after party selection. Changed dimens.

--- app/src/main/java/com/team2/packpackmonsters/PackAdapter.java
+++ app/src/main/java/com/team2/packpackmonsters/PackAdapter.java
@@ -24,12 +24,10 @@ public class PackAdapter extends BaseAdapter
     {
         this.context = context;
 
-        float density = context.getResources().getDisplayMetrics().density;
-
-        PADDING = (int) (context.getResources().getDimension(R.dimen.pack_padding) / density);
-        IMG_SIZE = (int) (context.getResources().getDimensionPixelSize(R.dimen.pack_img_size) / density);
-        MARGIN = (int) (context.getResources().getDimension(R.dimen.pack_margin) / density);
-        TEXT_SIZE = context.getResources().getDimension(R.dimen.pack_text_size) / density;
+        PADDING = (int) (context.getResources().getDimension(R.dimen.pack_padding));
+        IMG_SIZE = (int) (context.getResources().getDimension(R.dimen.pack_img_size));
+        MARGIN = (int) (context.getResources().getDimension(R.dimen.pack_margin));
+        TEXT_SIZE = context.getResources().getDimension(R.dimen.pack_text_size) / context.getResources().getDisplayMetrics().scaledDensity;
     }
 
     @Override
