2a089761e6490575743b13a3b104c5e61e2885f9
Brandon Selzer <bselzer1@hotmail.com>
2018-04-30 09:23:35 -0400

    Added toast for when the user clicks "Help." Removed monsterSelected now that party selection is forced. Party's current HP updates properly. Added alert dialog for party and item selection confirmation. Changed the way alert dialogs are created (still produces same result) because the components are mostly the same.

--- app/src/main/java/com/team2/packpackmonsters/HelpActivity.java
+++ app/src/main/java/com/team2/packpackmonsters/HelpActivity.java
@@ -2,6 +2,7 @@ package com.team2.packpackmonsters;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class HelpActivity extends AppCompatActivity
 {
@@ -14,6 +15,7 @@ public class HelpActivity extends AppCompatActivity
         setTitle(R.string.help_activity_title);
 
         //TEMPORARY
+        Toast.makeText(this, "This has not been implemented yet.", Toast.LENGTH_LONG).show();
         finish();
     }
 }
